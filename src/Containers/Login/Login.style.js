35f6e76160beea5ca3d8d4216811d7561430599c
Xavier Thufail <xavier.thufail@btpn.com>
2021-12-04 13:24:51 +0700

    feature(login): create hooks ho handle logic in component and styles to contain all styles in component

---
+++ src/Containers/Login/Login.style.js
@@ -0,0 +1,22 @@
+const styles = {
+  container: {
+    height: '100vh',
+    width: '100vw',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  logo: {
+    width: '200px'
+  },
+  login: {
+    textDecoration: 'none',
+    color: 'black',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    fontWeight: 'bold'
+  }
+};
+
+export default styles;
