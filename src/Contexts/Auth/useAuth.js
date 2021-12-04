d5e29a38235c3eaa77fd4bf5bc5ec91fd1e94560
Xavier Thufail <xavier.thufail@btpn.com>
2021-12-04 15:34:23 +0700

    feature(main): add call dispatch action when logout

--- src/Contexts/Auth/useAuth.js
+++ src/Contexts/Auth/useAuth.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+
+import allAction from "../../Store/Actions";
 
 const useAuth = () => {
   const [isLogin, setLogin] = React.useState(false);
+  const dispatch = useDispatch();
 
   const login = (user) => {
     localStorage.setItem('user', JSON.stringify(user));
@@ -11,13 +15,15 @@ const useAuth = () => {
   const logout = () => {
     setLogin(false);
     localStorage.removeItem('user');
+    dispatch(allAction.user.resetUsers())
   };
-
+  
   React.useEffect(() => {
     const user = localStorage.user;
-
+    
     if(user) {
       setLogin(true);
+      dispatch(allAction.user.setUser(JSON.parse(user)))
     }
   }, []);
 
