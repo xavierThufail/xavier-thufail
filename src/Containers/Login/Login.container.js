2673083fe79446f6e9dfad0597cd3c70ef335e7a
Xavier Thufail <xavier.thufail@btpn.com>
2021-12-04 13:22:29 +0700

    feature(login): create component for login oAuth Github

--- src/Containers/Login/Login.container.js
+++ src/Containers/Login/Login.container.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import LoginComponent from './Login.component';
+import { Loading } from '../../HOC';
+import useLogin from './Login.hook';
 import config from './Login.config';
 
 const Login = (props) => {
-  return <LoginComponent {...props} />;
+  const hooks = useLogin(props);
+
+  return <LoginComponent {...hooks} />;
 };
 
 Login.displayName = config.container.displayName;
 
-export default Login;
+export default Loading(Login);
